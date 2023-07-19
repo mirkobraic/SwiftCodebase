e83f5e3e230e85372537ef33454fd7b2c41105a8
Mirko Braic <mbraic@profico.hr>
2023-07-19 22:04:05 +0200

    Add SwiftUI project 3

---
+++ 100Days/SwiftUI/s03ViewsAndModifiers/s03ViewsAndModifiers/s03ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  s03ViewsAndModifiersApp.swift
+//  s03ViewsAndModifiers
+//
+//  Created by Mirko Braic on 19.07.2023..
+//
+
+import SwiftUI
+
+@main
+struct s03ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
