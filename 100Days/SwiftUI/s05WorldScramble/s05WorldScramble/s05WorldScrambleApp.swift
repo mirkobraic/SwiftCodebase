a725957ebe1502a45b3f93fe750dbea7308f86b1
Mirko Braic <mbraic@profico.hr>
2023-07-21 14:39:40 +0200

    Add SwiftUI project 5

---
+++ 100Days/SwiftUI/s05WorldScramble/s05WorldScramble/s05WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  s05WorldScrambleApp.swift
+//  s05WorldScramble
+//
+//  Created by Mirko Braic on 21.07.2023..
+//
+
+import SwiftUI
+
+@main
+struct s05WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
