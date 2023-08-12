04e1e2df2ef87907b567232296d5b945f0ce4c32
Mirko Braic <mbraic@profico.hr>
2023-08-12 15:31:46 +0200

    Add landmarks project

---
+++ Projects/Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,91 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Mirko Braic on 11.08.2023..
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+    @State private var selectedLandmark: Landmark?
+
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+
+        var id: FilterCategory { self }
+    }
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite) &&
+            (filter == .all || filter.rawValue == landmark.category.rawValue)
+        }
+    }
+
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
+    }
+
+    var index: Int? {
+        modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id })
+    }
+
+    var body: some View {
+        NavigationView {
+            List(selection: $selectedLandmark) {
+                ForEach (filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                    .tag(landmark)
+                }
+            }
+            .navigationTitle(title)
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                              ForEach(FilterCategory.allCases) { category in
+                                  Text(category.rawValue).tag(category)
+                              }
+                          }
+                        .pickerStyle(.inline)
+
+                        Toggle(isOn: $showFavoritesOnly) {
+                            Label("Favorites only", systemImage: "star.fill")
+                        }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+
+            Text("Select a Landmark")
+        }
+        .focusedValue(\.selectedLandmark, $modelData.landmarks[index ?? 0])
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+
+//        ForEach(["iPhone SE (3rd generation)", "iPhone XS Max", "iPad (10th generation)"], id: \.self) { deviceName in
+//            LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+    }
+}
---
+++ Projects/Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Mirko Braic on 12.08.2023..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
